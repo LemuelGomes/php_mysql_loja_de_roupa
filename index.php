dde480ec3815745276e6c375c97111a42fe563af
LEMUEL CAMPOS MENEZES GOMES <lemuel0072004@gmail.com>
2023-08-15 22:05:57 -0300

    Começo do Projeto

--- index.php
+++ index.php
@@ -1,41 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<?php
+    
+    include 'includes/header.php';
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+?>
 
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
+<?php
+    
+    include 'includes/footer.php';
 
-    <div id="login">
-        <p id="login-titulo"> Bem-vindo </p>
-
-        <form action="login.php" method="POST">
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email</label>
-                <input type="email" class="form-control" name="campo_email" id="exampleInputEmail1" aria-describedby="emailHelp">
-            </div>
-
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Senha</label>
-                <input type="password" class="form-control" name="campo_senha" id="exampleInputPassword1">
-            </div>
-
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Mantenha-me Logado</label>
-            </div>
-
-            <button type="submit" class="btn btn-success">Entrar</button>
-        
-          </form>
-    </div>
-
-</body>
-</html>
+?>
--- sistema.php
+++ sistema.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-
-</head>
-<body>
 
 <div class="row">
   <div class="col">
@@ -38,12 +26,12 @@
     <input type="text" class="form-control" id="inputAddress">
   </div>
   
-  <div class="col-4">
+  <div class="col-2">
     <label for="inputAddress2" class="form-label">Departamento</label>
     <input type="text" class="form-control" id="inputAddress2">
   </div>
   
-  <div class="col-md-6">
+  <div class="col-md-4">
     <label for="inputCity" class="form-label">City</label>
     <input type="text" class="form-control" id="inputCity">
   </div>
@@ -55,10 +43,12 @@
       <option>...</option>
     </select>
   </div>
+  
   <div class="col-md-2">
     <label for="inputZip" class="form-label">Zip</label>
     <input type="text" class="form-control" id="inputZip">
   </div>
+  
   <div class="col-12">
     <div class="form-check">
       <input class="form-check-input" type="checkbox" id="gridCheck">
@@ -67,10 +57,9 @@
       </label>
     </div>
   </div>
+  
   <div class="col-12">
     <button type="submit" class="btn btn-primary">Sign in</button>
   </div>
 </form>
 
-</body>
-</html>
